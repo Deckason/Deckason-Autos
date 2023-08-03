3d605646a654fbd4059a047e43328d96c65ed8b7
Deckason <chibuikemchidera@gmail.com>
2023-08-03 22:53:12 +0100

    [fix] converted thepost-product page to serverside and removed evry other client render related something

--- app/post-product/page.jsx
+++ app/post-product/page.jsx
@@ -1,17 +1,14 @@
-"use client"
 import { useRouter } from "next/navigation";
-import { useAppContext } from "../context/ContextProvider";
 import ProductForm from "./ProductForm/ProductForm";
 import { authentication } from "../utils/firebaseConfiguration";
 
 const PostProduct = () => {
-    const { isVerified} = useAppContext()
-    const {push} = useRouter()
-    console.log(authentication.currentUser)
+    // const { isVerified} = useAppContext()
+    // const {push} = useRouter()
+    // console.log(authentication.currentUser)
     return (
         <>
-            {authentication.currentUser?<ProductForm />:push("/auth")}
-            {/* {!isVerified && <h1>Not Yet Verified</h1>} */}
+            <ProductForm />
         </>
     );
 }
