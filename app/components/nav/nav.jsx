af093cbfb14a196123a08b01adc55254079394e9
Deckason <chibuikemchidera@gmail.com>
2023-08-14 13:33:08 +0100

    [add] Added background color to the bottom nav icons and sdded color to the top nav when clicked

--- app/components/nav/nav.jsx
+++ app/components/nav/nav.jsx
@@ -1,28 +1,32 @@
 "use client"
-import { signOut } from "firebase/auth";
 import styles from "./nav.module.css"
 import Link from "next/link";
 import logo from "../../../public/media/car logos/logo.jpg"
 import { useState } from "react";
-import { GiHamburgerMenu } from "react-icons/gi";
-import { MdClose } from "react-icons/md";
-import { authentication } from "@/app/utils/firebaseConfiguration";
 import Image from "next/image";
 
 const Nav = () => {
-   
+    const [activeBtn, setActiveBtn] = useState(null)
     return (
         <>
             <header className={styles.header}>
-                <div className={styles.header_logo}>
+                <div className={styles.header_logo} onClick={()=>setActiveBtn("homeBtn") }>
                     <Link href={"/"}><Image src={logo} width={300} height={100}/></Link>
                 </div>
                 <ul
                     className={`${styles.header_ul}`}>
-                    <li><Link href={"/"}>Home</Link></li>
-                    <li><Link href={"/nigerian"}>Nigerian Used</Link></li>
-                    <li><Link href={"/foreign"}>Foreign Used</Link></li>
-                    <li><Link href={"/about"}>About Us</Link></li>
+                    <li onClick={()=>setActiveBtn("homeBtn") }>
+                        <Link href={"/"} className={`${activeBtn === "homeBtn" ? styles.active:""}`}>Home</Link>
+                    </li>
+                    <li onClick={()=>setActiveBtn("nigerianBtn") }>
+                        <Link href={"/nigerian"} className={`${activeBtn === "nigerianBtn" ? styles.active:""}`}>Nigerian Used</Link>
+                    </li>
+                    <li onClick={()=>setActiveBtn("foreignBtn") }>
+                        <Link href={"/foreign"}  className={`${activeBtn === "foreignBtn" ? styles.active:""}`}>Foreign Used</Link>
+                    </li>
+                    <li onClick={()=>setActiveBtn("newtBtn") }>
+                        <Link href={"/new"}  className={`${activeBtn === "newtBtn" ? styles.active:""}`}>New</Link>
+                    </li>
                 </ul>
                 <div className={styles.header_search}>
                     <div className={styles.search_wrapper}>
